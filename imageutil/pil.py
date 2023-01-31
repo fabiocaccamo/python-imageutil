75f204b7bdd8d81e2e903c477d6b217ca27428aa
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-01-31 22:35:02 +0100

    Increase `PIL.ImageFile.MAXBLOCK` size.

--- imageutil/pil.py
+++ imageutil/pil.py
@@ -1,7 +1,11 @@
 from PIL import Image as PILImage
+from PIL import ImageFile as PILImageFile
 from PIL.Image import Image as PILImageObject
 
 __all__ = [
     "PILImage",
+    "PILImageFile",
     "PILImageObject",
 ]
+
+PILImageFile.MAXBLOCK = 1024 * 1024
